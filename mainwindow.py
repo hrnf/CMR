65df67c8b998269e1d9390d18249f2942cb1cb97
hrnf <hrn.sgo@gmail.com>
2015-09-26 11:27:31 -0300

    Added Canvas, and OK and Cancel buttons in layouts

--- mainwindow.py
+++ mainwindow.py
@@ -8,20 +8,27 @@ from PyQt5.QtWidgets import *
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.CMS_MW_init()
+        self.CMR_MW_init()
 
     def CMR_MW_init(self):
-        self.resize(300,300)
-        self.move(300,300)
-        self.setWindowTitle('Tittle test')
-        self.CMS_MW_addButtons()
-        self.show()
+        canvas = QGraphicsView()
+        okButton = QPushButton("OK")
+        cancelButton = QPushButton("Cancel")
 
-    def CMR_MW_addButtons(self):
-        b = QPushButton('Boton', self)
-        b.setToolTip('test')
-        b.resize(b.sizeHint())
-        b.move(50,50)
+        buttonsLayout = QHBoxLayout()
+        buttonsLayout.addStretch(1)
+        buttonsLayout.addwidget(okButton)
+        buttonsLayout.addwidget(cancelButton)
+
+        mainLayout = QVBoxLayout()
+        mainLayout.addStretch(1)
+        mainLayout.addWidget(canvas)
+        mainLayout.addLayout(buttonsLayout)
+
+        self.setLayout(mainLayout)
+        self.setGeometry(300, 300, 300, 150)
+        self.setWindowTitle('Community\'s Manga Reader')
+        self.show()
 
     def CMR_centerToScreen(self):
         qr = self.frameGeometry()
