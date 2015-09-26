671e10cb2e1c53771f5427532a2a4b6f3f435c84
hrnf <hrn.sgo@gmail.com>
2015-09-26 11:08:30 -0300

    Rename methods from CMS to CMR

--- mainwindow.py
+++ mainwindow.py
@@ -10,20 +10,20 @@ class MainWindow(QWidget):
         super().__init__()
         self.CMS_MW_init()
 
-    def CMS_MW_init(self):
+    def CMR_MW_init(self):
         self.resize(300,300)
         self.move(300,300)
         self.setWindowTitle('Tittle test')
         self.CMS_MW_addButtons()
         self.show()
 
-    def CMS_MW_addButtons(self):
+    def CMR_MW_addButtons(self):
         b = QPushButton('Boton', self)
         b.setToolTip('test')
         b.resize(b.sizeHint())
         b.move(50,50)
 
-    def CMS_centerToScreen(self):
+    def CMR_centerToScreen(self):
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
