10861ff6019c3127fc3e49269777afd5bb3df453
hrn <hrn@nihocielo>
2016-01-12 13:33:38 -0300

    Create working dirs. Add GUI style constants. Add samples

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
-
+import os
 from mainwindow import MainWindow
 
 class CMR():
+    userdir = os.path.expanduser("~/.CMR")
+
+    if not os.path.exists(userdir):
+        os.makedirs(userdir)
+        os.makedirs(userdir+"/releases")
+
+    MainWindow.make_list()
     mainwindow = MainWindow()
---
+++ styleconstants.py
@@ -0,0 +1,3 @@
+class StyleConstants():
+    CATALOGUE_PADDING = 3
+    CATALOGUE_IMAGE_SIZE = (250,250)
